bb771778a0f1454b9a7b7a5e99e3dd64d23b3448
Maharam Ali Khan <157357587+ps-updates@users.noreply.github.com>
2025-07-18 07:02:29 +0530

    Create __init__.py

---
+++ bot/__init__.py
@@ -0,0 +1,70 @@
+import sys, asyncio
+from datetime import datetime
+from pyrogram import Client
+from logger import LOGGER
+from info import Config
+
+ascii_art = """
+████████╗██╗░░██╗███████╗  ██████╗░░██████╗  ██████╗░░█████╗░████████╗░██████╗
+╚══██╔══╝██║░░██║██╔════╝  ██╔══██╗██╔════╝  ██╔══██╗██╔══██╗╚══██╔══╝██╔════╝
+░░░██║░░░███████║█████╗░░  ██████╔╝╚█████╗░  ██████╦╝██║░░██║░░░██║░░░╚█████╗░
+░░░██║░░░██╔══██║██╔══╝░░  ██╔═══╝░░╚═══██╗  ██╔══██╗██║░░██║░░░██║░░░░╚═══██╗
+░░░██║░░░██║░░██║███████╗  ██║░░░░░██████╔╝  ██████╦╝╚█████╔╝░░░██║░░░██████╔╝
+░░░╚═╝░░░╚═╝░░╚═╝╚══════╝  ╚═╝░░░░░╚═════╝░  ╚═════╝░░╚════╝░░░░╚═╝░░░╚═════╝░
+"""
+
+class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            name="PS-LinkVault",
+            api_id=Config.API_ID,
+            api_hash=Config.API_HASH,            
+            plugins=dict(root="bot/plugins"),
+            workers=Config.BOT_WORKERS,
+            bot_token=Config.BOT_TOKEN
+        )
+
+        self.log = LOGGER
+
+    async def start(self):
+        await super().start()
+        me = await self.get_me()
+        self.username = me.username
+        self.mention = me.mention
+        self.uptime = datetime.now()
+
+        if Config.FORCE_SUB_CHANNEL:
+            try:
+                link = (await self.get_chat(Config.FORCE_SUB_CHANNEL)).invite_link
+                if not link:
+                    await self.export_chat_invite_link(Config.FORCE_SUB_CHANNEL)
+                    link = (await self.get_chat(Config.FORCE_SUB_CHANNEL)).invite_link
+                self.invitelink = link
+            except Exception as a:
+                self.log(__name__).warning(a)
+                self.log(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
+                self.log(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {Config.FORCE_SUB_CHANNEL}")
+                self.log(__name__).info("\nBot Stopped. Join https://t.me/ps_discuss for support")
+                sys.exit()
+        try:
+            db_channel = await self.get_chat(Config.CHANNEL_ID)
+            self.db_channel = db_channel
+            test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
+            await test.delete()
+        except Exception as e:
+            self.log(__name__).warning(e)
+            self.log(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {Config.CHANNEL_ID}")
+            self.log(__name__).info("\nBot Stopped. Join https://t.me/ps_discuss for support")
+            sys.exit()
+
+        print(ascii_art)
+        self.log(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/ps_updates")
+        print("""Welcome to File Sharing Bot""")
+
+        if Config.WEB_MODE:
+            from web import start_webserver
+            asyncio.create_task(start_webserver(self, Config.PORT))
+
+    async def stop(self, *args):
+        await super().stop()
+        self.log(__name__).info("Bot stopped.")
