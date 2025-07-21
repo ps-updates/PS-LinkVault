5a922c42cdcbab48dbdbecafc00b2abd5ea661fd
Maharam Ali Khan <157357587+ps-updates@users.noreply.github.com>
2025-07-21 08:57:46 +0530

    ✨ Added token verification

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -2,6 +2,7 @@ from .messages import (
     get_messages, 
     get_message_id
 )
-from .scheduler import get_readable_time, schedule_auto_delete
+from .scheduler import schedule_auto_delete
+from .helper import get_readable_time, get_shortlink
 from .subscription import is_subscribed, handle_force_sub
 from .encoder import encode, decode
