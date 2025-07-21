9a7f2e22312f36d2e5f6f957d7db9e4b34167340
Maharam Ali Khan <157357587+ps-updates@users.noreply.github.com>
2025-07-21 09:00:44 +0530

    ✨ Added token verification

--- bot/plugins/stats.py
+++ bot/plugins/stats.py
@@ -11,7 +11,10 @@ async def stats(bot: Client, message: Message):
     time = get_readable_time(delta.seconds)
     await message.reply(Config.BOT_STATS_TEXT.format(uptime=time))
 
+'''
+#Not need If You comment out this one 
+#then you will face issues on token verification
 @Client.on_message(filters.private & filters.incoming)
 async def useless(_,message: Message):
     if Config.USER_REPLY_TEXT:
-        await message.reply(Config.USER_REPLY_TEXT)
+        await message.reply(Config.USER_REPLY_TEXT)'''
