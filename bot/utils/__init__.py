aa61eba2827c87ac81b599e27bc2ff53e7f5759a
Maharam Ali Khan <157357587+ps-updates@users.noreply.github.com>
2025-07-27 07:14:46 +0530

    Update __init__.py

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -2,7 +2,7 @@ from .messages import (
     get_messages, 
     get_message_id
 )
-from .scheduler import schedule_auto_delete
+from .scheduler import schedule_manager
 from .helper import get_readable_time, get_shortlink
-from .subscription import is_subscribed, handle_force_sub
+from .subscription import handle_force_sub
 from .encoder import encode, decode
